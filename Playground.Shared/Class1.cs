10abb2b1cc15e93f7125fc13be716171a411541c
Green Hornet <taphousecarl@gmail.com>
2022-03-11 13:20:34 -0500

    initial commit

---
+++ Playground.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Playground.Shared;
+public class Class1
+{
+
+}
---
+++ Playground.Shared/Playground.cs
@@ -0,0 +1,6 @@
+
+public class Play {
+	public int Id { get; set; }
+	public string Name { get; set; } = default!;
+	public string Description { get; set; } = default!;
+}
